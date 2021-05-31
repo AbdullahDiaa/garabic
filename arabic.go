ade256c3df057d7fe83139f30e3f2fa9ad8d88f5
abdullah diaa <abdullah.diaa@gmail.com>
2021-05-31 20:03:32 +0300

    Initial implementation of harakat removal && arabic text normalization for search

--- arabic.go
+++ arabic.go
@@ -1,2 +1,100 @@
 //Package arabic ..
 package arabic
+
+import (
+	"bytes"
+)
+
+const (
+	/*
+		Normalizable Arabic letters
+	*/
+
+	//Alef  => ا
+	Alef = '\u0627'
+	//AlefMad =>  آ
+	AlefMad = '\u0622'
+	//AlefHamzaAbove => أ
+	AlefHamzaAbove = '\u0623'
+	//AlefHamzaBelow إ
+	AlefHamzaBelow = '\u0625'
+	//Yae =>  ي
+	Yae = '\u064A'
+	//DotlessYae =>  ى
+	DotlessYae = '\u0649'
+	//TehMarbuta => ة
+	TehMarbuta = '\u0629'
+	//Hae => ه
+	Hae = '\u0647'
+	//Tatweel => ـ
+	Tatweel = '\u0640'
+
+	/*
+		Arabic Harakat (Harakat تَشْكِيل)
+	*/
+	Fatḥah     = '\u064E'
+	Kasrah     = '\u0650'
+	Dammah     = '\u064F'
+	DaggerAlif = '\u0670'
+	Sukun      = '\u0652'
+
+	TanwinFatḥah = '\u064B'
+	TanwinDammah = '\u064C'
+	TanwinKasrah = '\u064D'
+	Shaddah      = '\u0651'
+	//AlefWaslah ٱ / Waslah is considered part of harakat/تَشْكِيل ?
+	AlefWaslah = '\u0671'
+)
+
+//RemoveHarakat ...
+func RemoveHarakat(input string) string {
+	runes := bytes.Runes([]byte(input))
+	for i := 0; i < len(runes); i++ {
+		//fmt.Println(string(runes[i]))
+		switch runes[i] {
+		// diacritics
+		case DaggerAlif, TanwinKasrah, TanwinDammah, TanwinFatḥah, Fatḥah, Dammah, Kasrah, Shaddah, Sukun:
+			//Delete the matching diacritic while preserving order
+			runes = deleteRune(runes, i)
+			i--
+		//Remove Waslah from AlefWaslah
+		case AlefWaslah:
+			runes[i] = Alef
+		}
+	}
+	return string(runes)
+}
+
+//Normalize ..
+func Normalize(input string) string {
+	input = RemoveHarakat(input)
+	runes := bytes.Runes([]byte(input))
+	for i := 0; i < len(runes); i++ {
+		//fmt.Println(string(runes[i]))
+		switch runes[i] {
+		// diacritics
+		case Tatweel:
+			//Delete the matching diacritic while preserving order
+			runes = deleteRune(runes, i)
+			i--
+		//Normalizable letters
+		case AlefMad, AlefHamzaAbove, AlefHamzaBelow:
+			runes[i] = Alef
+		case DotlessYae:
+			runes[i] = Yae
+		case TehMarbuta:
+			runes[i] = Hae
+		}
+	}
+	//@TODO: optimize runes by converting it to bytes, arabic letters use only 2 bytes
+	return string(runes)
+}
+
+//deleteRune will delete a rune from the slice while keeping the order of runes
+func deleteRune(runes []rune, i int) []rune {
+	if i >= len(runes) {
+		return runes
+	}
+	runes = append(runes[:i], runes[i+1:]...)
+	return runes
+}
--- arabic_test.go
+++ arabic_test.go
@@ -1,1 +1,196 @@
 package arabic
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+const succeed = "\u2705"
+const failed = "\u274C"
+
+//TestRemoveHarakat ...
+func TestRemoveHarakat(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       string
+		expected    string
+	}{
+		{
+			description: "Removing Alif Khanjariyah",
+			input:       "رَحْمَٰن",
+			expected:    "رحمن",
+		},
+		{
+			description: "Removing Waslah",
+			input:       "ٱمْشُوا",
+			expected:    "امشوا",
+		},
+
+		{
+			description: "Removing all harakat 1",
+			input:       "يَا أَيُّهَا الَّذِينَ آمَنُوا أَوْفُوا بِالْعُقُودِ",
+			expected:    "يا أيها الذين آمنوا أوفوا بالعقود",
+		},
+		{
+			description: "Removing all harakat 2",
+			input:       "سَنواتٌ قَليلةٌ وأَدْخُلُ الجامِعَةَ، يا لها مِنْ رِحْلةٍ شاقَّةٍ طَويلَةٍ، ما أصعبَ أيامَ الدِّراسَةِ",
+			expected:    "سنوات قليلة وأدخل الجامعة، يا لها من رحلة شاقة طويلة، ما أصعب أيام الدراسة",
+		},
+		{
+			description: "Removing all harakat 3",
+			input:       "إِنِّني أَشْكُرُ رَبِّي دائماً، لكنني مُنْزعجةٌ.. أَلاَ يَحِقُّ لِيَ التعبيرُ عن ضِيقِ صَدْري؟!",
+			expected:    "إنني أشكر ربي دائما، لكنني منزعجة.. ألا يحق لي التعبير عن ضيق صدري؟!",
+		},
+	}
+
+	t.Log("Given an arabic string it should be normalized")
+	{
+		for i, tt := range testCases {
+			normalized := RemoveHarakat(tt.input)
+			t.Logf("\tTest: %d\t Normalizing %s", i, tt.input)
+			if normalized != tt.expected {
+				t.Errorf("\t%s\t(%s)\tShould be normalized to %s, got %s instead", failed, tt.description, tt.expected, normalized)
+			} else {
+				t.Logf("\t%s\t(%s)\tShould be normalized to %s", succeed, tt.description, tt.expected)
+			}
+		}
+	}
+}
+
+//TestNormalize ...
+func TestNormalize(t *testing.T) {
+	testCases := []struct {
+		description string
+		input       string
+		expected    string
+	}{
+		{
+			description: "AlefHamzaAbove removal from beginning of word",
+			input:       "أحمد",
+			expected:    "احمد",
+		},
+		{
+			description: "AlefHamzaAbove removal from middle of word",
+			input:       "مأمون",
+			expected:    "مامون",
+		},
+		{
+			description: "AlefHamzaAbove removal from end of word",
+			input:       "نمأ",
+			expected:    "نما",
+		},
+		{
+			description: "Replacing DotlessYae with Yae",
+			input:       "منى",
+			expected:    "مني",
+		},
+		{
+			description: "Replacing TehMarbuta with Hae",
+			input:       "مكتبة",
+			expected:    "مكتبه",
+		},
+		{
+			description: "Trimming Tatweel",
+			input:       "بريـــــــــد",
+			expected:    "بريد",
+		},
+		{
+			description: "Removing Maddah",
+			input:       "قُرْآن",
+			expected:    "قران",
+		},
+		{
+			description: "Normalizing Alif Waslah",
+			input:       "ٱمْشُوا",
+			expected:    "امشوا",
+		},
+		{
+			description: "Removing all harakat combined with text normalization 1",
+			input:       "يَا أَيُّهَا الَّذِينَ آمَنُوا أَوْفُوا بِالْعُقُودِ",
+			expected:    "يا ايها الذين امنوا اوفوا بالعقود",
+		},
+		{
+			description: "Removing all harakat combined with text normalization 2",
+			input:       "سَنواتٌ قَليلةٌ وأَدْخُلُ الجامِعَةَ، يا لها مِنْ رِحْلةٍ شاقَّةٍ طَويلَةٍ، ما أصعبَ أيامَ الدِّراسَةِ",
+			expected:    "سنوات قليله وادخل الجامعه، يا لها من رحله شاقه طويله، ما اصعب ايام الدراسه",
+		},
+		{
+			description: "Removing all harakat combined with text normalization 3",
+			input:       "إِنِّني أَشْكُرُ رَبِّي دائماً، لكنني مُنْزعجةٌ.. أَلاَ يَحِقُّ لِيَ التعبيرُ عن ضِيقِ صَدْري؟!",
+			expected:    "انني اشكر ربي دائما، لكنني منزعجه.. الا يحق لي التعبير عن ضيق صدري؟!",
+		},
+	}
+
+	t.Log("Given an arabic string it should be normalized")
+	{
+		for i, tt := range testCases {
+			normalized := Normalize(tt.input)
+			t.Logf("\tTest: %d\t Normalizing %s", i, tt.input)
+			if normalized != tt.expected {
+				t.Errorf("\t%s\t(%s)\tShould be normalized to %s, got %s instead", failed, tt.description, tt.expected, normalized)
+			} else {
+				t.Logf("\t%s\t(%s)\tShould be normalized to %s", succeed, tt.description, tt.expected)
+			}
+		}
+	}
+}
+
+//TestDeleteRune ...
+func TestDeleteRune(t *testing.T) {
+
+	testCases := []struct {
+		description string
+		input       []rune
+		index       int
+		expected    []rune
+	}{
+		{
+			description: "Deleting rune with index 0 that exists in the array",
+			input:       []rune{'a', 'b'},
+			index:       0,
+			expected:    []rune{'b'},
+		},
+		{
+			description: "Deleting rune with index 3 that exists in the array",
+			input:       []rune{'a', 'b', 'c', 'd', 'e'},
+			index:       3,
+			expected:    []rune{'a', 'b', 'c', 'e'},
+		},
+		{
+			description: "Deleting rune with index 0 that doesn't exist in the array",
+			input:       []rune{},
+			index:       0,
+			expected:    []rune{},
+		},
+		{
+			description: "Deleting rune with index 0 that's more than array length",
+			input:       []rune{'a', 'v'},
+			index:       10,
+			expected:    []rune{'a', 'v'},
+		},
+	}
+
+	t.Log("Given a slice of runes and a position of a rune, it should be deleted from the slice while keeping order")
+	{
+		for i, tt := range testCases {
+			input := tt.input
+			input = deleteRune(input, tt.index)
+			t.Logf("\tTest: %d\t Deleting rune %d from %v", i, tt.index, tt.input)
+			if !reflect.DeepEqual(input, tt.expected) {
+				t.Errorf(
+					"\t%s\t(%s)\tShould be %v (len %d, cap %d), got %v (len %d, cap %d) instead",
+					failed, tt.description, tt.expected, len(tt.expected), cap(tt.expected), input, len(input), cap(input),
+				)
+			} else {
+				t.Logf("\t%s\t(%s)\tShould be %v", succeed, tt.description, tt.expected)
+			}
+		}
+	}
+}
+func ExampleNormalize() {
+	normalized := Normalize("أحمد")
+	fmt.Println(normalized)
+	// Output:
+	// احمد
+}
